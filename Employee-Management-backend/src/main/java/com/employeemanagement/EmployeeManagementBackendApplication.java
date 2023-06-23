8d4f6231501a1990cbfd5806e1a55656e7b43ce2
Rohit9252 <rohitbatra0165@gmail.com>
2023-06-23 17:19:52 +0530

    swagger added for backend

--- Employee-Management-backend/src/main/java/com/employeemanagement/EmployeeManagementBackendApplication.java
+++ Employee-Management-backend/src/main/java/com/employeemanagement/EmployeeManagementBackendApplication.java
@@ -3,6 +3,8 @@ package com.employeemanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Swagger url : - http://localhost:8080/swagger-ui/index.html#/employee-controller
+
 @SpringBootApplication
 public class EmployeeManagementBackendApplication {
 
